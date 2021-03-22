2141aad901668c2fcefb82a140c012a94ed4e763
tarik <tarik.moussa@gt-arc.com>
2021-03-22 09:33:56 +0100

    clean git

--- terraform/modules/experiment/modules/attacker/output.tf
+++
@@ -1,5 +0,0 @@
-# Finally, we're outputting the IP address of the new attacker vm
-output "ip_address" {
- value = "${vsphere_virtual_machine.vm.default_ip_address}"
-  description = "The Remote desktop IP address of the attackker instance."
-}
