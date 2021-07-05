6589afd72de64e1c04c51d2662b75ec1f51da8e5
tarik <tarik.moussa@gt-arc.com>
2021-07-06 00:43:52 +0200

    clean up

--- terraform/modules/remote/remote.tf
+++ terraform/modules/remote/remote.tf
@@ -3,8 +3,8 @@ module "controller" {
     connection_host     = local.control_ip[0]
     connection_user     = local.control_username[0]
     connection_type     = "ssh"
-   
-    remote_exec_command =  "sudo apt-get install python3 python3-pip sshpass -y && sudo pip3 install lxml " 
+                            //install ansible prerequisites
+    remote_exec_command =  "sudo apt-get install python3 python3-pip sshpass -y &&  pip3 install lxml " 
                                             
     path_to_playbook    = "dummyPlaybook.yml"
 }
